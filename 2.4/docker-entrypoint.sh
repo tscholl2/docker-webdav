202e818d163947e5f25906264b14eb9d49086987
travis <traviswscholl@gmail.com>
2022-01-01 17:56:58 -0500

    change www-data user id to match that used by backing file system

--- 2.4/docker-entrypoint.sh
+++ 2.4/docker-entrypoint.sh
@@ -101,6 +101,7 @@ fi
 # Create directories for Dav data and lock database.
 [ ! -d "/var/lib/dav/data" ] && mkdir -p "/var/lib/dav/data"
 [ ! -e "/var/lib/dav/DavLock" ] && touch "/var/lib/dav/DavLock"
-chown -R www-data:www-data "/var/lib/dav"
+usermod -u 1001 www-data # TODO: get user id from docker argument set at build time using name of group/user we want
+#chown -R www-data:www-data "/var/lib/dav"
 
 exec "$@"
